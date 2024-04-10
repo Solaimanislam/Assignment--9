415a32d83e7c5c28897d0fd10ef35b5f2b6be910
Solaimanislam <islamsi2001@gmail.com>
2024-04-11 00:25:16 +0600

    added navbar section

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,13 +1,13 @@
 import Navbar from "../../Shared/Navbar/Navbar";
 
 
-const Home = () => {
+const Register = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <h2 className="text-3xl">This is Home pages...</h2>
+            <h2 className="text-3xl">This is Register page...</h2>
         </div>
     );
 };
 
-export default Home;
+export default Register;
